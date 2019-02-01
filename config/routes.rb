5dc17511fd852e5c63740bdf8c2bb7830cd0b3ca
Saint-Jean Germila <germila@noukod.com>
2019-02-01 17:59:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-resources :students, except: [:destroy, :index]
-resources :school_classes, except: [:destroy, :index]
+resources :students
+# , except: [:destroy, :index]
+resources :school_classes
+# , except: [:destroy, :index]
 #   resources :students, only: [:index, :show, :new, :create, :edit]
 #       patch 'students/:id', to: 'students#update'
 #
